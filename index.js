f1bf655e507b68733be3297ce6c899dd51d643f3
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-09-09 21:01:41 +0600

    post 'user'

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epxwefd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,24 +30,56 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db('bristDB').collection('menu');
+    const userCollection = client.db('bristDB').collection('users');
     const reviewCollection = client.db('bristDB').collection('reviews');
     const cartCollection = client.db('bristDB').collection('cart');
     
+    // users
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      console.log(user);
+      const existingUser = await userCollection.find(query).toArray();
+      if(existingUser){
+        return res.send({message: 'user already exist'});
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // menu
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
 
+
+    // reviews
     app.get('/reviews', async(req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     })
 
     // cart
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([])
+      }
+      const query = {email : email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
     app.post('/carts', async(req, res) => {
       const item = req.body;
-      // console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
